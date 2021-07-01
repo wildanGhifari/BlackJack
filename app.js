c556060299fb37a7d44c53060f410d3401d7514d
wildanGhifari <wildghifari@gmail.com>
2021-07-01 22:09:57 +0700

    Create basic if..else statement for Blackjack

---
+++ app.js
@@ -0,0 +1,17 @@
+// Variable Declaration
+let firstCard = 6
+let secondCard = 15
+let sum = firstCard + secondCard
+let hasBlackjack = false
+let isAlive = true
+let message = ""
+
+if (sum < 21) {
+    message = "Do you want to draw a new card?"
+} else if (sum === 21) {
+    message = "You've got Blackjack!"
+    hasBlackjack = true
+} else {
+    message = "You're out of the game!"
+    isAlive = false
+}
