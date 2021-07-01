9f29454f0fc44ddb6fe65594c22f6d448e4b8a67
wildanGhifari <wildghifari@gmail.com>
2021-07-01 23:07:38 +0700

    Display the card element and add New Card Button

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-
+let cardsEl = document.getElementById("cards-el")
 
 
 function startGame(){
+    cardsEl.textContent += " " + firstCard + " " + secondCard
     sumEl.textContent += " " + sum
+
+    // Set condition if sum less than 21 or exactly equal 21 or greater than 21.
     if (sum < 21) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -23,4 +26,8 @@ function startGame(){
     }
     
     messageEl.textContent = message
+}
+
+function newCard() {
+    console.log("Drawing a new card!")
 }
